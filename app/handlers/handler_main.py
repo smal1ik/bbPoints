e11ac076829108a8de890dcdd712532e50e6ca93
Matvey <m.karymov@irkpk.ru>
2024-10-24 18:56:04 +0800

    fix

--- app/handlers/handler_main.py
+++ app/handlers/handler_main.py
@@ -287,11 +287,8 @@ async def answer_message(message: types.Message, state: FSMContext, bot: Bot):
 
 @router_main.message(F.chat.id == ID_CHAT, F.text, F.reply_to_message, F.from_user.is_bot == False)  # ID ЧАТА
 async def answer_message(message: types.Message, state: FSMContext, bot: Bot, arqredis: ArqRedis):
-    print(message)
     user = await get_user(message.from_user.id)
-    print(message.reply_to_message.message_id, list_channel_message)
-    print(type(message.reply_to_message.message_id), type(list_channel_message[0]))
-    if (message.reply_to_message.message_id in list_channel_message) and user and not user.send_comment:
+    if (message.forward_from_message_id in list_channel_message) and user and not user.send_comment:
         await bot.send_message(message.from_user.id, "Ты оставил комментарий, получай 10 баллов")
         api.add_points(message.from_user.id, 10)
         await user_send_comment(message.from_user.id)
