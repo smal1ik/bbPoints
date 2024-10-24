d20dd551eae5c00fc699fca18fdd9ce1b5cc5653
Matvey <m.karymov@irkpk.ru>
2024-10-24 17:42:15 +0800

    share fix text

--- app/keyboards/keyboard_main.py
+++ app/keyboards/keyboard_main.py
@@ -43,7 +43,7 @@ sure_btn = sure_btn.as_markup()
 
 
 def get_menu_btn(ref: str):
-    text = "Присоединяйся%20в%20бота"
+    text = "%D0%9F%D1%80%D0%B8%D1%81%D0%BE%D0%B5%D0%B4%D0%B8%D0%BD%D1%8F%D0%B9%D1%81%D1%8F%20%D0%B2%20%D0%B1%D0%BE%D1%82%D0%B0"
     url = f"t.me/smallik1_bot?start={ref}"
     menu_btn = InlineKeyboardBuilder()
     # menu_btn.row(
