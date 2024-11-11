99c3124150452e1de1db948b20f6eb40fc5de88e
Matvey <m.karymov@irkpk.ru>
2024-11-11 17:51:50 +0800

    add stats

--- app/fns/fns_api.py
+++ app/fns/fns_api.py
@@ -64,13 +64,33 @@ def send_message_request(token, sum, date, fn, fiscal_document_id, fiscal_sign):
         return None
 
 
+a = {'id': 3612821491523884288, 'ofdId': 'ofd26', 'receiveDate': '2019-12-15T14:21:51Z', 'subtype': 'receipt',
+ 'address': '140404, Московская обл, Коломна г, Гаврилова ул, дом № 1,пом.181', 'content': {
+    'rawData': 'AwCdAhEEEAA5Mjg1MDAwMTAwMTUwMTc1DQQUADAwMDA1NDg5NjcwMTkzMzggICAg+gMMADIzMTAwMzE0NzUgIBAEBABCkwAA9AMEAEBr9l01BAYAMQQX3W/YDgQEAPIAAAASBAQAIAEAAB4EAQAB/AMDAEwLASMEVgAGBDAAQkVBVVRZIEJPTUIggaul4aogpKvvIKPjoSBCRkYg4jAyKIOgrKygIIqu4ayl4qiqNwQCAExA/wMCAAABEwQCAExArwQBAAGwBAIAtwq+BAEABCMEVgAGBDAAQkVBVVRZIEJPTUIgj6CrpeKqoCDipa2lqSBFeWVzaGFkb3cgQkZGIDAxIChaaGVqNwQCAJhY/wMCAAABEwQCAJhYrwQBAAGwBAIAxA6+BAEABCMEVgAGBDAAQkVBVVRZIEJPTUIglaCpq6Cp4qXgIERpYW1vbmQgQkZGIOIwMShaaGVqaWFuZyk6NwQCAChY/wMCAAABEwQCAChYrwQBAAGwBAIAsQ6+BAEABCMEVgAGBDAAU0tJTkxJVEUgjKDhqqAgrqyuq6CmIK8vo6ugpyDhIKquq6ugoyAzMOjiKJ2koqitNwQCAEAa/wMCAAABEwQCAEAarwQBAAGwBAIAYAS+BAEABAcEAQAAOQQDAEwLAb8EAQAAwAQBAADBBAEAAP0DGQCSrqKg4K6ipaQgrKCjoKeoraAgmKjorqKgHwQBAAEkBAwAd3d3Lm5hbG9nLnJ1owQUAIygo62o4iCKruGspeKoqiCRoOKoGAQLAICOICKSoK2kpeAi8QNAADE0MDQwNCwgjK7hqq6i4aqg7yCuoassIIquq66sraAgoywgg6Ci4KirrqKgIOOrLCCkrqwg/CAxLK+urC4xODG5BAEAAk4EAgCNLIEG+v5N7DFn',
+    'messageFiscalSign': 9297394450888929639, 'code': 3, 'fiscalDriveNumber': '9285000100150175',
+    'kktRegId': '0000548967019338', 'userInn': '2310031475', 'fiscalDocumentNumber': 37698, 'dateTime': 1576430400,
+    'fiscalSign': 400388056, 'shiftNumber': 242, 'requestNumber': 288, 'operationType': 1, 'totalSum': 68428, 'items': [
+        {'name': 'BEAUTY BOMB Блеск для губ BFF т02(Гамма Косметик', 'price': 16460, 'quantity': 1.0, 'sum': 16460,
+         'nds': 1, 'ndsSum': 2743, 'paymentType': 4},
+        {'name': 'BEAUTY BOMB Палетка теней Eyeshadow BFF 01 (Zhej', 'price': 22680, 'quantity': 1.0, 'sum': 22680,
+         'nds': 1, 'ndsSum': 3780, 'paymentType': 4},
+        {'name': 'BEAUTY BOMB Хайлайтер Diamond BFF т01(Zhejiang):', 'price': 22568, 'quantity': 1.0, 'sum': 22568,
+         'nds': 1, 'ndsSum': 3761, 'paymentType': 4},
+        {'name': 'SKINLITE Маска омолаж п/глаз с коллаг 30шт(Эдвин', 'price': 6720, 'quantity': 1.0, 'sum': 6720,
+         'nds': 1, 'ndsSum': 1120, 'paymentType': 4}], 'cashTotalSum': 0, 'ecashTotalSum': 68428, 'prepaidSum': 0,
+    'creditSum': 0, 'provisionSum': 0, 'operator': 'Товаровед магазина Шишова', 'appliedTaxationType': 1,
+    'fnsUrl': 'www.nalog.ru', 'retailPlace': 'Магнит Косметик Сати', 'user': 'АО "Тандер"',
+    'retailPlaceAddress': '140404, Московская обл, Коломна г, Гаврилова ул, дом № 1,пом.181',
+    'fiscalDocumentFormatVer': 2, 'nds18': 11405}}
+
+
 def get_items_from_result(result):
     result = result['soap:Envelope']['soap:Body']['GetMessageResponse']['Message']['GetTicketResponse'][
         'Result']['Ticket']
     result = json.loads(result)
     items = result['content']['items']
     res_items = []
-    print(result)
+    print(result['content']['retailPlace'])
     for item in items:
         res_items.append({'name': item['name'], 'price': item['price']})
     return res_items
