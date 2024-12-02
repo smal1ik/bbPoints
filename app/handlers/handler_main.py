60be39673a6dac41725bdfed332c1c5124e513dd
Matvey <m.karymov@irkpk.ru>
2024-12-03 00:26:26 +0800

    add command info user

--- app/handlers/handler_main.py
+++ app/handlers/handler_main.py
@@ -47,7 +47,7 @@ class AntiManyReply(BaseMiddleware):
         self.cache.add(event.chat.id)
         try:
             result = await handler(event, data)
-
+            print(data)
             return result
         except Exception as e:
             print(e)
